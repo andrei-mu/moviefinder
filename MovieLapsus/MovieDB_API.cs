1796324271c9c423feea2d8166bd6149bd9075de
Andrei Muraru <andrei5008@hotmail.com>
2014-10-10 19:56:24 +0000

    added project

---
+++ MovieLapsus/MovieDB_API.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace imdbmov
+{
+    public class MovieDB_API_RAW
+    {
+        private static string API_KEY = "7d3315bb7234145c8d3b6e4b89e6ec55";
+        private static string LISTS = "http://api.themoviedb.org/3/person/{ACTOR_ID}/movie_credits?api_key={APIKEY}";
+        private static string ACTOR_QUERY = "http://api.themoviedb.org/3/search/person?api_key={APIKEY}&query={ACTOR_NAME}";
+        private static string STRING_TEST_QUERY = "http://www.google.com";
+
+        private string ActorSearchQuery
+        {
+            get
+            {
+                return ACTOR_QUERY;
+            }
+        }
+
+        private string ActorInfoQuery
+        {
+            get
+            {
+                return LISTS;
+            }
+        }
+
+        private async Task<string> GenericHTTPQuery(string query)
+        {
+            var link = query.Replace("{APIKEY}", API_KEY);
+
+            var request = System.Net.WebRequest.Create(link) as System.Net.HttpWebRequest;
+
+            request.Method = "GET";
+            string responseContent = "";
+            string responseError = "";
+
+            try
+            {
+                var reqStreamTask = request.GetResponseAsync();
+                using (var response = await reqStreamTask)
+                {
+                    using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                responseError += "\nWeb exception raised!\n";
+                responseError += "Message: ";
+                responseError += e.Message;
+                responseError += "\nStatus: ";
+                responseError += e.Status;
+                responseError += "\n";
+
+                System.Diagnostics.Debug.WriteLine(responseError);
+            }
+            catch (Exception e)
+            {
+                responseError += "\nException raised!\n";
+                responseError += "\nMessage: ";
+                responseError += e.Message;
+                responseError += "\n";
+
+                System.Diagnostics.Debug.WriteLine(responseError);
+            }
+
+            return responseContent;
+
+        }
+
+        public async Task<string> SearchForActor(string actorName)
+        {
+            actorName = actorName.Replace(" ", "+");
+            string query = ActorSearchQuery.Replace("{ACTOR_NAME}", actorName);
+
+            string actorResponse = await this.GenericHTTPQuery(query);
+            return actorResponse;
+        }
+
+        public async Task<string> GetActorInfoFromID(string actorID)
+        {
+            string query = ActorInfoQuery.Replace("{ACTOR_ID}", actorID);
+
+            string actorResponse = await this.GenericHTTPQuery(query);
+            return actorResponse;
+        }
+
+    }
+}
---
+++ MovieLapsusTest/UnitTest_MovieDB_API_RAW.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace imdbmov_testapp
+{
+    [TestClass]
+    public class UnitTest_MovieDB_API_RAW
+    {
+        [TestMethod]
+        public async Task Test_SearchForActor_List()
+        {
+            var api = new imdbmov.MovieDB_API_RAW();
+
+            string ret = await api.SearchForActor("pitt");
+
+            Assert.IsTrue(ret.Contains("Brad Pitt"));
+            Assert.IsTrue(ret.Contains("Jacob Pitts"));
+            Assert.IsTrue(ret.Contains("Michael Pitt"));
+            Assert.IsTrue(ret.Contains("Ernst Pittschau"));
+        }
+
+        [TestMethod]
+        public async Task Test_SearchForActor_ID()
+        {
+            var api = new imdbmov.MovieDB_API_RAW();
+
+            string ret = await api.SearchForActor("brad pitt");
+
+            Assert.IsTrue(ret.Contains("\"id\":287"));
+            Assert.IsTrue(ret.Contains("Fight Club"));
+        }
+
+        [TestMethod]
+        public async Task Test_ActorMovieList()
+        {
+            var api = new imdbmov.MovieDB_API_RAW();
+
+            string ret = await api.GetActorInfoFromID("287");
+
+            Assert.IsTrue(ret.Contains("Fight Club"));
+            Assert.IsTrue(ret.Contains("Legends of the Fall"));
+            Assert.IsTrue(ret.Contains("Too Young to Die?"));
+            Assert.IsTrue(ret.Contains("The Curious Case of Benjamin Button"));
+            Assert.IsTrue(ret.Contains("Sinbad: Legend of the Seven Seas"));
+            Assert.IsTrue(ret.Contains("Johnny Suede"));
+            
+            
+        }
+    }
+}
