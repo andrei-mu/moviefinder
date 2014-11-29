d243534273da49fb92ee20e5ba417bee48ca7b82
Andrei Muraru <andrei5008@hotmail.com>
2014-11-29 20:41:03 +0000

    Movie class

---
+++ MovieLapsus/TMDB/TMDB_Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLapsus
+{
+    namespace ACMOV
+    {
+        public class TMDB_Character
+        {
+            public int CastId {get; set; }
+            public string Character {get; set; }
+            public int ActorId {get; set; }
+            public string Name {get; set; }
+            public string ImagePath {get; set; }
+
+            private TMDB_Character(int actorId)
+            {
+                ActorId = actorId;
+            }
+
+            public static TMDB_Character CreateCharacter(int actorId)
+            {
+                var character = new TMDB_Character(actorId);
+
+                return character;
+            }
+        }
+    };
+};
